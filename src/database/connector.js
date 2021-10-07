9942ab224ad2ea8cee69aa10aebe776aa6156b9f
KaiAtDCI <kaiatdci@web.de>
2021-10-07 14:54:14 +0200

    added mongoose connection event handlers

--- src/database/connector.js
+++ src/database/connector.js
@@ -6,18 +6,19 @@ import { Course } from "./models/Course.js"; export { Course };
 import { Book } from "./models/Book.js"; export { Book };
 
 export async function connect() {
-  try {
-    const connectionString = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOSTNAME}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-    await mongoose.connect(connectionString);
-    console.log(`Connected to DB '${process.env.DB_NAME}'.`)
-  } catch (error) {
-    console.log(error.message);
-  }
+  const connectionString = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOSTNAME}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+  await mongoose.connect(connectionString);
 }
 
 export async function disconnect() {
   await mongoose.disconnect();
 }
+
+mongoose.connection.on('error', (err) => console.log(">> Error ", err) || process.exit(0));
+mongoose.connection.on('connecting', () => console.log(`>> Connecting to ${process.env.DB_NAME}`))
+mongoose.connection.on('connected', () => console.log(">> Connected"))
+mongoose.connection.on('disconnecting', () => console.log(">> Disconnecting"))
+mongoose.connection.on('disconnected', () => console.log(">> Disconnected"))
 
 export async function deleteAllCollectionEntries() {
   await Person.deleteMany();
